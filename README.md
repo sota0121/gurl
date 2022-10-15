63739725678cdff119bd4e29f3144f17db73452f
sota_masuda <sota.masuda@the-lionsdata.com>
2022-10-15 15:04:57 +0900

    update README so that equal to cURL help message

--- README.md
+++ README.md
@@ -6,20 +6,19 @@ This is a simple cURL emulator written in Go. It is not intended to be a full re
 
 ```
 $ gurl -h
-Usage of curl:
-  -d string
-        HTTP request body
-  -h    Show this help message
-  -i    Include HTTP response headers
-  -m string
-        HTTP method (default "GET")
-  -o string
-        Write HTTP response body to file
-  -v    Verbose output
-  -x string
-        HTTP proxy
-  -X string
-        HTTP method (default "GET")
+Usage: gurl [options...] <url>
+ -d, --data <data>          HTTP POST data
+ -f, --fail                 Fail silently (no output at all) on HTTP errors
+ -h, --help <category>      Get help for commands
+ -i, --include              Include protocol response headers in the output
+ -o, --output <file>        Write to file instead of stdout
+ -O, --remote-name          Write output to a file named as the remote file
+ -s, --silent               Silent mode
+ -T, --upload-file <file>   Transfer local FILE to destination
+ -u, --user <user:password> Server user and password
+ -A, --user-agent <name>    Send User-Agent <name> to server
+ -v, --verbose              Make the operation more talkative
+ -V, --version              Show version number and quit
 ```
 
 ## Examples
--- cmd/main.go
+++ cmd/main.go
@@ -13,18 +13,17 @@ func main() {
 	fmt.Println("--------------------------------")
 	fmt.Println("This is ", cmdName, " command")
 	fmt.Println("--------------------------------")
-	fmt.Println(`Usage of gurl:
-	-d string
-		  HTTP request body
-	-h    Show this help message
-	-i    Include HTTP response headers
-	-m string
-		  HTTP method (default "GET")
-	-o string
-		  Write HTTP response body to file
-	-v    Verbose output
-	-x string
-		  HTTP proxy
-	-X string
-		  HTTP method (default "GET")`)
+	fmt.Println(`Usage: gurl [options...] <url>
+	-d, --data <data>          HTTP POST data
+	-f, --fail                 Fail silently (no output at all) on HTTP errors
+	-h, --help <category>      Get help for commands
+	-i, --include              Include protocol response headers in the output
+	-o, --output <file>        Write to file instead of stdout
+	-O, --remote-name          Write output to a file named as the remote file
+	-s, --silent               Silent mode
+	-T, --upload-file <file>   Transfer local FILE to destination
+	-u, --user <user:password> Server user and password
+	-A, --user-agent <name>    Send User-Agent <name> to server
+	-v, --verbose              Make the operation more talkative
+	-V, --version              Show version number and quit`)
 }
