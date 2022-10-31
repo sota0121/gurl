1351cd86a16b858f03416585482182276bbd4c74
sota_masuda <sota.masuda@the-lionsdata.com>
2022-10-31 21:42:02 +0900

    update command_test.go and all tests became green

--- cmd/feature/command_test.go
+++ cmd/feature/command_test.go
@@ -1,19 +1,104 @@
 package feature
 
 import (
+	"flag"
 	"net/http"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestGurlCommand_Execute(t *testing.T) {
+var (
+	progname = "gurl"
+)
+
+type IntOpt struct { // implements for flag.Value interface
+	value int
+	name  string
+}
+
+func (o *IntOpt) Set(value string) error {
+	converted, err := strconv.Atoi(value)
+	if err != nil {
+		return err
+	}
+	o.value = converted
+	return nil
+}
+
+func (o IntOpt) String() string {
+	return strconv.Itoa(o.value)
+}
+
+type StringOpt struct { // implements for flag.Value interface
+	value string
+	name  string
+}
+
+func (o *StringOpt) Set(value string) error {
+	o.value = value
+	return nil
+}
+
+func (o StringOpt) String() string {
+	return o.value
+}
+
+type BoolOpt struct { // implements for flag.Value interface
+	value bool
+	name  string
+}
+
+func (o *BoolOpt) Set(value string) error {
+	converted, err := strconv.ParseBool(value)
+	if err != nil {
+		return err
+	}
+	o.value = converted
+	return nil
+}
+
+func (o BoolOpt) String() string {
+	return strconv.FormatBool(o.value)
+}
+
+// setCommandlineArgs sets the commandline arguments for testing
+func setCommandlineArgs(t *testing.T, progname string, args []string) {
+	t.Helper() // mark this function as a helper function
+
+	os.Args = append([]string{progname}, args...)
+}
+
+// setCommandlineOptions sets the commandline options for testing
+func setCommandlineOptions(t *testing.T, opts []flag.Value) {
+	t.Helper() // mark this function as a helper function
+
+	for _, opt := range opts {
+		switch opt.(type) {
+		case *IntOpt:
+			flag.CommandLine.Set(opt.(*IntOpt).name, opt.(*IntOpt).String())
+		case *StringOpt:
+			flag.CommandLine.Set(opt.(*StringOpt).name, opt.(*StringOpt).String())
+		case *BoolOpt:
+			flag.CommandLine.Set(opt.(*BoolOpt).name, opt.(*BoolOpt).String())
+		default:
+			t.Fatalf("unknown option type: %T", opt)
+		}
+	}
+}
+
+func TestNewCommand(t *testing.T) {
+	setCommandlineArgs(t, progname, []string{"GET", "http://example.com"})
+	testoptions := make([]flag.Value, 0, 3)
+	testoptions = append(testoptions, &StringOpt{value: "hello", name: "data"})
+	testoptions = append(testoptions, &BoolOpt{value: true, name: "verbose"})
+	setCommandlineOptions(t, testoptions)
+
 	c := NewCommand()
 	require.Equal(t, ReqContext{method: http.MethodGet}, *c.ctx)
 	require.Equal(t, CmdConfig{}, *c.cfg)
 	require.Equal(t, "dummy", c.client)
 	require.Equal(t, "Usage: gurl [options...] <url>", c.usage, "usage should be equal")
-
-	err := c.Execute()
-	require.NoError(t, err)
 }
