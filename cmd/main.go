f5c926b968c5ce7ec0b3a501832e44fd77a91831
sota_masuda <sota.masuda@the-lionsdata.com>
2022-11-02 11:28:47 +0900

    rm unused codes

--- cmd/main.go
+++ cmd/main.go
@@ -11,27 +11,4 @@ func main() {
 	if err := command.Execute(); err != nil {
 		fmt.Println(err)
 	}
-
-	// var cmdVar string
-	// flag.StringVar(&cmdVar, "cmd", "default", "cmd desu")
-	// flag.Parse()
-	// fmt.Println("cmdVar is : ", cmdVar)
-
-	// cmdName := cmdName()
-	// fmt.Println("--------------------------------")
-	// fmt.Println("This is ", cmdName, " command")
-	// fmt.Println("--------------------------------")
-	// fmt.Println(`Usage: gurl [options...] <url>
-	// -d, --data <data>          HTTP POST data
-	// -f, --fail                 Fail silently (no output at all) on HTTP errors
-	// -h, --help <category>      Get help for commands
-	// -i, --include              Include protocol response headers in the output
-	// -o, --output <file>        Write to file instead of stdout
-	// -O, --remote-name          Write output to a file named as the remote file
-	// -s, --silent               Silent mode
-	// -T, --upload-file <file>   Transfer local FILE to destination
-	// -u, --user <user:password> Server user and password
-	// -A, --user-agent <name>    Send User-Agent <name> to server
-	// -v, --verbose              Make the operation more talkative
-	// -V, --version              Show version number and quit`)
 }
